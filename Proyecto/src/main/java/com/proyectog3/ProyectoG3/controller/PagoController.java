d79f19af0535ffe0c526ad34e5de9b5195144cd8
Abigail Barrantes <barrantesabigail09@gmail.com>
2026-03-11 15:28:05 -0600

    Historia 9 pagos new

---
+++ Proyecto/src/main/java/com/proyectog3/ProyectoG3/controller/PagoController.java
@@ -0,0 +1,14 @@
+package com.proyectog3.ProyectoG3.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PagoController {
+
+    @GetMapping("/pago")
+    public String mostrarPago() {
+        return "general/pago";
+    }
+
+}
